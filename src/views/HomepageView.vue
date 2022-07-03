664e0a2faa6b1741606539e9cfccf7cac006cd03
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-07-03 21:14:09 +0700

    refactor:

---
+++ src/views/HomepageView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <h2>welcomeee</h2>
+      <div class="absolute w-full h-screen bg-zinc-700"></div>
+    </div>
+  </main>
+</template>
